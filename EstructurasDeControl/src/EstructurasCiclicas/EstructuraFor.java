ad1dfd8736e0ea825c66aa797d57c8780664d58f
NoeGarcia2022 <noegarcia4789@gmail.com>
2022-04-18 16:58:27 -0600

    Ejercicios Prácticos ParteII  Estructuras Ciclicas

---
+++ EstructurasDeControl/src/EstructurasCiclicas/EstructuraFor.java
@@ -0,0 +1,28 @@
+/*2-. Crear otra clase con el nombre EstructuraFor y desarrollaremos el ejemplo 
+anterior con esa estructura; el código será elsiguiente:*/
+
+package EstructurasCiclicas;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class EstructuraFor {
+    public static void main(String[] args) {
+        try {
+            BufferedReader leer = new BufferedReader (new InputStreamReader(System.in));
+            System.out.println("Ingresar un numero limite de repeticion: ");
+            String limite = leer.readLine();
+            //Casting o conversion de datos al momento de envio
+            instruccionFor(Integer.parseInt(limite));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void instruccionFor(int valorLimite) {
+        //for(valor inicial; valor final; incremento)
+        for(int i = 1; i <= valorLimite; i++){
+            System.out.println("El valor de la repeticion es: " + i);
+        }
+    }
+}
---
+++ EstructurasDeControl/src/EstructurasCiclicas/EstructuraWhile.java
@@ -0,0 +1,34 @@
+/*1-. Crear otro paquete dentro del mismo proyecto que se viene trabajando con 
+el nombre estructuras.ciclicas en ese paquete crear una clase con el nombre 
+EstructuraWhile y el código será el siguiente:*/
+
+package EstructurasCiclicas;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class EstructuraWhile {
+    public static void main(String[] args) {
+        try {
+            BufferedReader leer = new BufferedReader (new InputStreamReader(System.in));
+            System.out.println("Ingresar un numero limite de repeticion: ");
+            String limite = leer.readLine();
+            if(limite == null){ //evalua si el valor String no esta vacio
+                System.out.println("Dato invalido");
+            }else{
+                //Envio de parametros al metodo correspondiente haciendo la conversion o casting de datos
+                instruccionWhileSencillo(Integer.parseInt(limite));
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public static void instruccionWhileSencillo(int valorLimite) {
+        int i=1; //valor inicial del contador 
+        while(i <= valorLimite){ //Repetir mientras no se cumpla la condicion 
+            System.out.println("El valor de la repeticion es: " + i);
+            i++; //Incremento de 1 por cada intereacion 
+        }
+    }
+}
